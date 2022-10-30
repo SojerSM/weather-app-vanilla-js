3468fc70fcd8253c1d37ced296e68d832c1e305e
SojerSM <seb.maz1996@gmail.com>
2022-10-30 20:41:37 +0100

    Working on state update

--- src/app/searchbox/searchbox-model.js
+++ src/app/searchbox/searchbox-model.js
@@ -1,13 +1,28 @@
 import { API_KEY, API_DIRECT_URL } from '../config';
 
+const state = {
+  latitude: 0,
+  longitude: 0,
+  name: '',
+  state: '',
+};
+
 export const loadLocation = async function (cityName) {
   try {
     const request = await fetch(
       `${API_DIRECT_URL}q=${cityName}&appid=${API_KEY}`
     );
     const data = await request.json();
-    console.log(data);
+    updateState(data[0]);
   } catch (err) {
     console.log(`Error: ${err}`);
   }
+  console.log(state);
+};
+
+const updateState = function (data) {
+  state.latitude = data.lat;
+  state.longitude = data.lon;
+  state.name = data.name;
+  state.state = data.state;
 };
