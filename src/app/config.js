98f9f517d49ed6d2dc6b12c0d1cee6e7f9d506d5
SojerSM <seb.maz1996@gmail.com>
2022-10-31 20:16:59 +0100

    Databox ready for now

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,3 @@
 export const API_KEY = '00f72020ac292c6e5bbd95e3b2db17f8';
 export const API_DIRECT_URL = 'http://api.openweathermap.org/geo/1.0/direct?';
+export const API_STD_URL = 'https://api.openweathermap.org/data/2.5/weather?';
